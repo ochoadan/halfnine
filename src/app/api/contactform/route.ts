b575b3fe53d35d7b6e63e4549e0276c57b34fd5f
Dan <42155753+ochoadan@users.noreply.github.com>
2023-09-22 13:39:49 -0400

    9/22/2023

---
+++ src/app/api/contactform/route.ts
@@ -0,0 +1,41 @@
+import nodemailer from 'nodemailer';
+
+export async function POST(request: Request, response: Response) {
+  const { name, email, phone, company, message } = await request.json();
+
+  let transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  });
+
+  let mailOptions = {
+    from: 'contact-form@halfnine.com',
+    to: 'diogamb@gmail.com',
+    subject: `Message From ${name}`,
+    text: message + " | Sent from: " + email,
+    html: `<div>${message}</div><p>Email: ${email}</p><p>Phone: ${phone}</p><p>Company: ${company}</p>`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+  return new Response('Success', {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  } catch (error) {
+    console.error('Error:', error);
+    return new Response('Error', {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,21 +1,29 @@
 "use client";
 
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { Turnstile } from "@marsidev/react-turnstile";
-import React, { useRef } from "react";
-import { useState } from "react";
-import {
-  FaRegUser,
-  FaRegEnvelope,
-  FaRegBuilding,
-  FaPhoneAlt,
-} from "react-icons/fa";
+import clsx from "clsx";
+import React, { useState, useEffect } from "react";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaRegBuilding, FaRegEnvelope, FaRegUser } from "react-icons/fa6";
 
-export default function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [company, setCompany] = useState("");
-  const [phone, setPhone] = useState("");
-  const [message, setMessage] = useState("");
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    company: "",
+    phone: "",
+    message: "",
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    name: false,
+    email: false,
+    message: false,
+  });
+
+  const [awaitingResponse, setAwaitingResponse] = useState(false);
+  const [messageSuccess, setMessageSuccess] = useState(false);
   const [submitted, setSubmitted] = useState(false);
 
   const [turnstileSolved, setTurnstileSolved] = useState(false);
@@ -23,21 +31,42 @@ export default function Contact() {
     setTurnstileSolved(true);
   };
 
-  const formRef = React.useRef<HTMLFormElement>(null);
-  async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
-    event.preventDefault();
-    const formData = new FormData(formRef.current!);
-    const token = formData.get("cf-turnstile-response");
-    const requestBody = {
-      token,
-      name,
-      email,
-      phone,
-      company,
-      message,
+  const emailRegex =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const nameRegex = /^[a-z ,.'-]+$/i;
+
+  useEffect(() => {
+    const newFormErrors = {
+      name: !nameRegex.test(formData.name),
+      email: !emailRegex.test(formData.email),
+      message: formData.message.trim() === "",
     };
+    setFormErrors(newFormErrors);
+  }, [formData, submitted]);
+
+  const handleInputChange = (e: any) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    setSubmitted(true);
+    setAwaitingResponse(true);
+
+    if (Object.values(formErrors).some((error) => error)) {
+      console.log("Form is invalid.");
+      setAwaitingResponse(false);
+      return;
+    }
+    const requestBody = { ...formData };
 
     try {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+
       const response = await fetch("/api/contactform", {
         method: "POST",
         headers: {
@@ -50,26 +79,33 @@ export default function Contact() {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
 
+      setMessageSuccess(true);
+
       const data = await response.json();
 
-      if (data.status === "success") {
-        alert("Email sent successfully!");
+      if (data.status === 200) {
+        setMessageSuccess(true);
+        return;
       } else {
         alert("Failed to send email.");
+        return;
       }
     } catch (error) {
-      console.error("Error:", error);
-      alert("An error occurred. Please try again.");
+      // console.error("Error:", error);
+      // alert("An error occurred. Please try again.");
+      return;
+    } finally {
+      setAwaitingResponse(false);
+      return;
     }
-  }
+  };
 
   return (
     <div className="py-16 md:py-24">
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="bg-slate-50 rounded-xl p-5 lg:p-10 shadow-sm">
           <h2 className="pb-2 text-2xl md:text-3xl font-bold">Contact Form:</h2>
-          {/* <form action={'submit'} onSubmit={handleSubmit}> */}
-          <form ref={formRef} onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit}>
             <div className="space-y-5">
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 w-full">
                 <div className="space-y-1">
@@ -77,16 +113,21 @@ export default function Contact() {
                     Name<span className="text-red-600 ml-1 select-none">*</span>
                   </label>
                   <div className="relative">
-                    <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <FaRegUser color="slate.800" />
+                    <span className="text-slate-600 absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <FaRegUser />
                     </span>
                     <input
+                      // required
                       type="text"
                       name="name"
-                      onChange={(e: any) => setName(e.target.value)}
-                      placeholder="John Doe"
-                      className="pl-10 block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
-                      required
+                      value={formData.name}
+                      onChange={handleInputChange}
+                      placeholder="Name"
+                      className={
+                        submitted && formErrors.name
+                          ? "pl-10 block w-full shadow-sm sm:text-sm border-red-300 rounded-md focus:ring-red-500"
+                          : "pl-10 block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
+                      }
                     />
                   </div>
                 </div>
@@ -96,23 +137,28 @@ export default function Contact() {
                     <span className="text-red-600 ml-1 select-none">*</span>
                   </label>
                   <div className="relative">
-                    <span className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-                      <FaRegEnvelope color="slate.800" />
+                    <span className="text-slate-600 absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                      <FaRegEnvelope />
                     </span>
                     <input
                       type="text"
-                      name="name"
-                      onChange={(e: any) => setEmail(e.target.value)}
+                      name="email"
+                      value={formData.email}
+                      onChange={handleInputChange}
                       placeholder="Email"
-                      className="pl-10 block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
-                      required
+                      // required
+                      className={
+                        submitted && formErrors.email
+                          ? "pl-10 block w-full shadow-sm sm:text-sm border-red-300 rounded-md focus:ring-red-500"
+                          : "pl-10 block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
+                      }
                     />
                   </div>
                 </div>
                 <div className="space-y-1">
                   <label>
                     Company Name
-                    <span className="text-gray-500 ml-2 select-none">
+                    <span className="text-slate-500 ml-2 select-none">
                       (Optional)
                     </span>
                   </label>
@@ -122,10 +168,10 @@ export default function Contact() {
                     </span>
                     <input
                       type="text"
-                      name="name"
-                      onChange={(e: any) => setCompany(e.target.value)}
-                      placeholder="HalfNine"
-                      required
+                      name="company"
+                      value={formData.company}
+                      onChange={handleInputChange}
+                      placeholder="Company"
                       className="pl-10 block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
                     />
                   </div>
@@ -133,7 +179,7 @@ export default function Contact() {
                 <div className="space-y-1">
                   <label>
                     Phone Number
-                    <span className="text-gray-500 ml-2 select-none">
+                    <span className="text-slate-500 ml-2 select-none">
                       (Optional)
                     </span>
                   </label>
@@ -143,10 +189,10 @@ export default function Contact() {
                     </span>
                     <input
                       type="text"
-                      name="name"
-                      onChange={(e: any) => setPhone(e.target.value)}
-                      placeholder="Phone Number"
-                      required
+                      name="phone"
+                      value={formData.phone}
+                      onChange={handleInputChange}
+                      placeholder="Phone"
                       className="pl-10 block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
                     />
                   </div>
@@ -158,25 +204,70 @@ export default function Contact() {
                   <span className="text-red-600 ml-1 select-none">*</span>
                 </label>
                 <textarea
-                  required
-                  onChange={(e: any) => setMessage(e.target.value)}
-                  id="message"
                   name="message"
-                  rows={4}
-                  aria-describedby="message-description"
-                  className="block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
-                  defaultValue=""
+                  value={formData.message}
+                  onChange={handleInputChange}
+                  placeholder="Message"
+                  className={
+                    submitted && formErrors.message
+                      ? "block w-full shadow-sm sm:text-sm border-red-300 rounded-md focus:ring-red-500"
+                      : "block w-full shadow-sm sm:text-sm border-slate-200 rounded-md focus:ring-sky-500"
+                  }
                 />
               </div>
+
+              {submitted &&
+                (formErrors.name || formErrors.email || formErrors.message ? (
+                  <div className="rounded-md bg-red-50 border-red-200 border-[1px]  px-4 py-3">
+                    <div className="flex">
+                      <div className="flex-shrink-0">
+                        <XCircleIcon
+                          className="h-5 w-5 text-red-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                      <div className="ml-3">
+                        <h3 className="text-sm font-medium text-red-800">
+                          There are errors in the form
+                        </h3>
+                        <div className="mt-2 text-sm text-red-700">
+                          <ul role="list" className="list-disc space-y-1 pl-5">
+                            {formErrors.name && (
+                              <li>You must include a valid Name.</li>
+                            )}
+                            {formErrors.email && (
+                              <li>The email you input is not a valid email.</li>
+                            )}
+                            {formErrors.message && (
+                              <li>You must include a Message.</li>
+                            )}
+                          </ul>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  messageSuccess && (
+                    <div className="rounded-md bg-green-50 border-green-200 border-[1px] px-4 py-3">
+                      <div className="flex">
+                        <div className="flex-shrink-0">
+                          <CheckCircleIcon
+                            className="h-5 w-5 text-green-400"
+                            aria-hidden="true"
+                          />
+                        </div>
+                        <div className="ml-3">
+                          <p className="text-sm font-medium text-green-800">
+                            Your Message was Submitted Successfully
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                ))}
               {!turnstileSolved && (
                 <>
-                  <div
-                    // pl={1}
-                    // fontSize={"sm"}
-                    // pb={1}
-                    // mt={-2}
-                    color={"gray.600"}
-                  >
+                  <div color={"slate.600"}>
                     Solve the Challenge to Submit the Form
                   </div>
                   <Turnstile
@@ -187,16 +278,42 @@ export default function Contact() {
               )}
               {turnstileSolved && (
                 <button
-                  // colorScheme={"blue"}
-                  type={"submit"}
-                  // isLoading={submitted}
-                  // loadingText={"Submitting"}
-                  className="rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
-                  onClick={(e: any) => {
-                    handleSubmit(e);
-                  }}
+                  type="submit"
+                  disabled={awaitingResponse}
+                  className={clsx(
+                    "bg-sky-600 flex rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-500",
+                    awaitingResponse &&
+                      "bg-slate-400 select-none hover:bg-slate-400"
+                  )}
                 >
-                  Submit Message
+                  {awaitingResponse && (
+                    <svg
+                      width="20"
+                      height="20"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="mr-2"
+                    >
+                      <path
+                        fill="#FFFFFF"
+                        d="M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,19a8,8,0,1,1,8-8A8,8,0,0,1,12,20Z"
+                        opacity=".25"
+                      />
+                      <path
+                        fill="#FFFFFF"
+                        d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z"
+                      >
+                        <animateTransform
+                          attributeName="transform"
+                          type="rotate"
+                          dur="0.75s"
+                          values="0 12 12;360 12 12"
+                          repeatCount="indefinite"
+                        />
+                      </path>
+                    </svg>
+                  )}
+                  {awaitingResponse ? "Submitting..." : "Submit Message"}
                 </button>
               )}
             </div>
@@ -205,4 +322,6 @@ export default function Contact() {
       </div>
     </div>
   );
-}
+};
+
+export default Contact;
