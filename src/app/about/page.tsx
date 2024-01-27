f110fb52f15d85fa570ec0da6738df57de095b19
Dan <42155753+ochoadan@users.noreply.github.com>
2024-01-27 12:39:21 -0500

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -132,7 +132,7 @@ const Page = () => {
                 </h2>
                 <p className="relative z-10 mt-2 text-sm text-zinc-500 dark:text-zinc-400">Status: <span className="text-zinc-700">{project.status}</span></p>
                 <p className="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">{project.description}</p>
-                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+                <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-sky-500 dark:text-zinc-200">
                   <LinkIcon className="h-6 w-6 flex-none" />
                   <span className="ml-2">{project.link.label}</span>
                 </p>
