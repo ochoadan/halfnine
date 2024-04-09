5d048af1a2eaf457f092ffa47fc5567161d504ee
Dan <diogamb@gmail.com>
2024-04-09 19:23:01 -0400

    4/9/2024

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Wix_Madefor_Display } from "next/font/google";
 const fontSource = Wix_Madefor_Display({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Technology Company Restructuring and Turnaround - Halfnine",
+  title: "Technology Company Restructuring and Turnaround Services - Halfnine",
   description:
     "We help you stand out if you are experiencing the effects of a declining sales, increased competition, or consolidating markets. We deliver success to the struggling.",
   alternates: { canonical: "https://www.halfnine.com" },
