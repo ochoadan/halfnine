4913cf04eb159d229438ecd6fac23a7e2653e0af
Dan <diogamb@gmail.com>
2024-04-12 18:58:05 -0400

    Sitemap Fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || "https://www.halfnine.com/",
   generateRobotsTxt: false,
   output: "standalone",
-  exclude: ["/contact", "/dev", "/contact/*", "/blog/*"],
+  exclude: ["/contact", "/dev", "/contact/*", "/blog/posts/*"],
   robotsTxtOptions: {
     additionalSitemaps: ["https://www.halfnine.com/blog/sitemap.xml"],
   },
