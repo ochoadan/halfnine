e078dd6964a11e64495cbb24907a2e4819e733e0
Dan <diogamb@gmail.com>
2024-04-12 13:10:51 -0400

    Update faq.tsx

--- src/app/(default)/(root)/faq.tsx
+++ src/app/(default)/(root)/faq.tsx
@@ -39,47 +39,43 @@ const GlobalFAQ = () => {
         <h2>Frequently asked questions:</h2>
         <hr className="border-sky-600 opacity-90 border-b-[2px] w-20 my-2" />
       </div>
-      <div className="mt-10 divide-y divide-gray-900/10">
+      <dl className="mt-10 divide-y divide-gray-900/10">
         {faqs.map((faq) => (
-          <div key={faq.question}>
-            <div>
-              <Disclosure as="div">
-                {({ open }) => (
-                  <>
-                    <div>
-                      <Disclosure.Button
-                        className={clsx(
-                          "flex w-full items-start justify-between text-left text-gray-700 hover:text-black",
-                          {
-                            "pt-6 pb-4": open,
-                            "py-6": !open,
-                          }
-                        )}
-                      >
-                        <span className="text-base font-semibold leading-7">
-                          {faq.question}
-                        </span>
-                        <span className="ml-6 flex h-7 items-center">
-                          {open ? (
-                            <MinusIcon className="h-6 w-6" aria-hidden="true" />
-                          ) : (
-                            <PlusIcon className="h-6 w-6" aria-hidden="true" />
-                          )}
-                        </span>
-                      </Disclosure.Button>
-                    </div>
-                    <div className="pr-12 mb-6">
-                      <p className="text-base leading-7 text-gray-600">
-                        {faq.answer}
-                      </p>
-                    </div>
-                  </>
-                )}
-              </Disclosure>
-            </div>
-          </div>
+          <Disclosure as="div" key={faq.question}>
+            {({ open }) => (
+              <>
+                <dt>
+                  <Disclosure.Button
+                    className={clsx(
+                      "flex w-full items-start justify-between text-left text-gray-700 hover:text-black",
+                      {
+                        "pt-6 pb-4": open,
+                        "py-6": !open,
+                      }
+                    )}
+                  >
+                    <span className="text-base font-semibold leading-7">
+                      {faq.question}
+                    </span>
+                    <span className="ml-6 flex h-7 items-center">
+                      {open ? (
+                        <MinusIcon className="h-6 w-6" aria-hidden="true" />
+                      ) : (
+                        <PlusIcon className="h-6 w-6" aria-hidden="true" />
+                      )}
+                    </span>
+                  </Disclosure.Button>
+                </dt>
+                <Disclosure.Panel className="pr-12 mb-6">
+                  <p className="text-base leading-7 text-gray-600">
+                    {faq.answer}
+                  </p>
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
         ))}
-      </div>
+      </dl>
     </div>
   );
 };
