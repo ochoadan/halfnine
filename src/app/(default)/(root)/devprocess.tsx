5aac72fe9326cd63ea60e5e8cfda0c1d40d3d490
Your Name <diogamb@gmail.com>
2024-04-26 16:50:28 -0400

    4/26/2024 - B

--- src/app/(default)/(root)/devprocess.tsx
+++ src/app/(default)/(root)/devprocess.tsx
@@ -1,55 +1,49 @@
-import { FadeIn, FadeInStagger } from "@/components/Global/FadeIn";
-
 export default function DevProcess() {
   return (
     <>
-      <FadeInStagger>
-        <FadeIn className="mx-auto lg:mx-0">
-          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            Want custom software?
-            <hr className="border-brand-600 opacity-90 border-b-[2px] w-24 my-2" />
-          </h2>
-          <p className="mt-4 text-base leading-7 text-gray-600">
-            General estimates for a wide range of software development tasks,
-            including mobile apps, web apps, and platform enhancements
-          </p>
-        </FadeIn>
-        <div className="mx-auto mt-16 flex flex-col gap-8 lg:mx-0 lg:mt-20 lg:flex-row lg:items-end">
-          <FadeIn className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-100 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
-            <p className="flex-none text-3xl font-bold text-gray-900">$15k+</p>
-            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-              <p className="text-2xl font-bold text-gray-900">Discovery</p>
-              <p className="mt-2 text-base leading-7 text-gray-600">
-                Gathering the essential elements and building MVP&apos;s
-              </p>
-            </div>
-          </FadeIn>
-          <FadeIn className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
-            <p className="flex-none text-3xl font-bold text-white">
-              $20k - $1M+
+      <div className="mx-auto lg:mx-0">
+        <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+          Want custom software?
+          <hr className="border-brand-600 opacity-90 border-b-[2px] w-24 my-2" />
+        </h2>
+        <p className="mt-4 text-base leading-7 text-gray-600">
+          General estimates for a wide range of software development tasks,
+          including mobile apps, web apps, and platform enhancements
+        </p>
+      </div>
+      <div className="mx-auto mt-16 flex flex-col gap-8 lg:mx-0 lg:mt-20 lg:flex-row lg:items-end">
+        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-100 p-8 sm:w-3/4 sm:max-w-md sm:flex-row-reverse sm:items-end lg:w-72 lg:max-w-none lg:flex-none lg:flex-col lg:items-start">
+          <p className="flex-none text-3xl font-bold text-gray-900">$15k+</p>
+          <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+            <p className="text-2xl font-bold text-gray-900">Discovery</p>
+            <p className="mt-2 text-base leading-7 text-gray-600">
+              Gathering the essential elements and building MVP&apos;s
             </p>
-            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-              <p className="text-2xl font-bold text-white">Development</p>
-              <p className="mt-2 text-base leading-7 text-gray-400">
-                Designing technology that is both reliable and ready
-                for deployment after testing
-              </p>
-            </div>
-          </FadeIn>
-          <FadeIn className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-brand-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
-            <p className="flex-none text-3xl font-bold text-white">
-              $5k - 10k+ / month
-            </p>
-            <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
-              <p className="text-2xl font-bold text-white">Maintenance</p>
-              <p className="mt-2 text-base leading-7 text-brand-200">
-                We build dynamic and adaptable solutions, with regular updates
-                and new features
-              </p>
-            </div>
-          </FadeIn>
+          </div>
         </div>
-      </FadeInStagger>
+        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-gray-900 p-8 sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-sm lg:flex-auto lg:flex-col lg:items-start lg:gap-y-44">
+          <p className="flex-none text-3xl font-bold text-white">$20k - $1M+</p>
+          <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+            <p className="text-2xl font-bold text-white">Development</p>
+            <p className="mt-2 text-base leading-7 text-gray-400">
+              Designing technology that is both reliable and ready for
+              deployment after testing
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col-reverse justify-between gap-x-16 gap-y-8 rounded-2xl bg-brand-600 p-8 sm:w-11/12 sm:max-w-xl sm:flex-row-reverse sm:items-end lg:w-full lg:max-w-none lg:flex-auto lg:flex-col lg:items-start lg:gap-y-28">
+          <p className="flex-none text-3xl font-bold text-white">
+            $5k - 10k+ / month
+          </p>
+          <div className="sm:w-80 sm:shrink lg:w-auto lg:flex-none">
+            <p className="text-2xl font-bold text-white">Maintenance</p>
+            <p className="mt-2 text-base leading-7 text-brand-200">
+              We build dynamic and adaptable solutions, with regular updates and
+              new features
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/app/(default)/(root)/offering.tsx
+++ src/app/(default)/(root)/offering.tsx
@@ -5,51 +5,53 @@ import Link from "next/link";
 export default function DevProcess() {
   return (
     <>
-      <FadeInStagger faster>
-        <FadeIn className="mx-auto lg:mx-0">
-          <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
-            Our Technical Capabilities
-            <hr className="border-brand-600 opacity-90 border-b-[2px] w-24 my-2" />
-          </h2>
-        </FadeIn>
-        <div className="divide-y divide-gray-200">
-          {categories.map((category, index) => (
-            <FadeIn className="py-4" key={index}>
-              <h3 className="text-base sm:text-lg font-extrabold text-gray-900">
-                {category.title}
-              </h3>
-              <ul role="list" className="mt-2 gap-4 flex flex-wrap">
-                {category.items.map((item, itemIdx) => (
-                  <li key={itemIdx}>
-                    <div className="group flex w-full items-center justify-between space-x-3 rounded-xl py-1 px-2 border border-gray-100 text-left bg-white shadow-sm shadow-gray-100">
-                      <span className="flex min-w-0 flex-1 items-center space-x-2">
-                        <span className="block flex-shrink-0 rounded-lg overflow-hidden">
-                          <item.icon className="h-6 w-6" />
-                        </span>
-                        <span className="block min-w-0 flex-1">
-                          <span className="block truncate text-sm font-medium text-gray-900">
-                            {item.name}
-                          </span>
+      <div className="mx-auto lg:mx-0">
+        <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl">
+          Our Technical Capabilities
+          <hr className="border-brand-600 opacity-90 border-b-[2px] w-24 my-2" />
+        </h2>
+      </div>
+      <div className="divide-y divide-gray-200">
+        {categories.map((category, index) => (
+          <div className="py-4" key={index}>
+            <h3 className="text-base sm:text-lg font-extrabold text-gray-900">
+              {category.title}
+            </h3>
+            <ul role="list" className="mt-2 gap-4 flex flex-wrap">
+              {category.items.map((item, itemIdx) => (
+                <li key={itemIdx}>
+                  <div className="group flex w-full items-center justify-between space-x-3 rounded-xl py-1 px-2 border border-gray-100 text-left bg-white shadow-sm shadow-gray-100">
+                    <span className="flex min-w-0 flex-1 items-center space-x-2">
+                      <span className="block flex-shrink-0 rounded-lg overflow-hidden">
+                        <item.icon className="h-6 w-6" />
+                      </span>
+                      <span className="block min-w-0 flex-1">
+                        <span className="block truncate text-sm font-medium text-gray-900">
+                          {item.name}
                         </span>
                       </span>
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </FadeIn>
-          ))}
-        </div>
-        <FadeIn className="pt-4 sm:pt-6 md:flex md:items-center">
-          <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
-            Find a solution not listed here? <span>We might work with it! </span><span>
-              <Link href="/contact" className="text-brand-600 hover:text-brand-500 hover:underline">
-                Contact us
-              </Link>
-            </span>
-          </h3>
-        </FadeIn>
-        {/* Find a solution not listed here? We might work with it! Contact us today. */}
-      </FadeInStagger >
+                    </span>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="pt-4 sm:pt-6 md:flex md:items-center">
+        <h3 className="text-lg font-bold text-gray-900 sm:text-xl">
+          Find a solution not listed here? <span>We might work with it! </span>
+          <span>
+            <Link
+              href="/contact"
+              className="text-brand-600 hover:text-brand-500 hover:underline"
+            >
+              Contact us
+            </Link>
+          </span>
+        </h3>
+      </div>
+      {/* Find a solution not listed here? We might work with it! Contact us today. */}
     </>
   );
 }
